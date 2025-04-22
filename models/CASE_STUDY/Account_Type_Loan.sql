ef484448dd0329638e17098a6361b0806cf2e6ec
Vani Biradar <vanbirad@in.ibm.com>
2025-04-22 11:32:28 +0000

    Commit

---
+++ models/CASE_STUDY/Account_Type_Loan.sql
@@ -0,0 +1,17 @@
+SELECT
+T."Transaction_ID",
+T."Customer_ID",
+T."Transaction_Amount",
+T."Transaction_Date",
+T."Transaction_Type",
+T."Transaction_Mode",
+B."ACCOUNTNUMBER",
+B."BRANCH",
+B."ACCOUNT_TYPE"
+FROM
+MYDB.DBT.TRANSACTION_XML as T
+JOIN
+MYDB.DBT.BANKING_DATA_JSON as B
+ON
+T."Customer_ID"=B."CUSTOMER_ID"
+where B."ACCOUNT_TYPE" = 'Loan'
