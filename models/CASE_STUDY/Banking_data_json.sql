bb0392bc99d4b3bb0662302810eb1a91adedf077
Vani Biradar <vanbirad@in.ibm.com>
2025-04-22 09:16:55 +0000

    Commit

---
+++ models/CASE_STUDY/Banking_data_json.sql
@@ -0,0 +1,12 @@
+SELECT
+_DATA:"CUSTOMER_ID"::NUMBER as CUSTOMER_ID,
+_DATA:"FIRST_NAME"::STRING as FIRST_NAME,
+_DATA:"MIDDLE_NAME"::STRING as MIDDLE_NAME,
+_DATA:"LAST_NAME"::STRING as LAST_NAME,
+_DATA:"ACCOUNT_TYPE"::STRING as ACCOUNT_TYPE,
+_DATA:"ACCOUNT_DETAILS"[0]:"Address":"Residential"::STRING as Residential,
+_DATA:"ACCOUNT_DETAILS"[0]:"Address":"Billing"::STRING as Billing,
+_DATA:"ACCOUNT_DETAILS"[0]:"Banking":"branch"::STRING as branch,
+_DATA:"ACCOUNT_DETAILS"[0]:"Banking":"accountNumber"::STRING as accountNumber,
+_DATA:"ACCOUNT_DETAILS"[0]:"Banking":"balance"::NUMBER as Phone_balance
+ FROM MYDB.AZURE_CASE_STUDY.JSON
