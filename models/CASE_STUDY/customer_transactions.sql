c52cd71294f09597a0aa70681343136b985e7a65
Vani Biradar <vanbirad@in.ibm.com>
2025-04-22 09:51:52 +0000

    Commit

---
+++ models/CASE_STUDY/customer_transactions.sql
@@ -0,0 +1,15 @@
+SELECT
+T."Transaction_ID",
+T."Customer_ID",
+T."Transaction_Amount",
+T."Transaction_Date",
+T."Transaction_Type",
+T."Transaction_Mode",
+B."ACCOUNTNUMBER",
+B."BRANCH"
+FROM
+MYDB.DBT.TRANSACTION_XML as T
+JOIN
+MYDB.DBT.BANKING_DATA_JSON as B
+ON
+T."Customer_ID"=B."CUSTOMER_ID"
